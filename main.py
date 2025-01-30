3dbb5f28d22b1fa2a32c51b706309999887cc23e
Antonin <antonin.chiarotto.1@ens.etsmtl.ca>
2025-01-30 16:38:25 -0500

    main

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-import string 
-
-cle = 3
-
-mot = "LE petit chaperon rouge"
-
-
-def dechiffrer_texte(mot,cle):
-    nouveau_mot=''
-    for char in mot:
-        if char in alphabet:
-            modif = alphabet.find(char) + cle
-            lettre = alphabet[modif]
-            nouveau_mot = nouveau_mot + lettre
-        else :
-            nouveau_mot = nouveau_mot + char
-
-    return nouveau_mot
+import string
 
 alphabet = string.ascii_lowercase
 
 
+
+cle = 3
+
+
+
+mot = "LE petit chaperon rouge"
+
 mot_minuscule = mot.lower()
 
-print(mot_minuscule)
 
 
 def fonction_chiffrage(cle, mot):
@@ -38,5 +27,20 @@ def fonction_chiffrage(cle, mot):
         texte_chiffre += new_character
     return texte_chiffre
 
-print(fonction_chiffrage(cle,mot_minuscule))
+
+def chiffrement_Cesar() :
+    cle = int(input("Entrez la clé : "))
+    if input("Avez vous un fichier texte ou une phrase ? (t/p): ") == 't':
+        titre_fichier = input("Entrez le nom du fichier : ")
+        #mot = lecture_fichier(titre_fichier)
+    else :
+        mot = str(input("Entrez la phrase : "))
+    if input("Voulez-vous chiffrer ou déchiffrer ? (c/d): ") == 'c':
+
+        print(fonction_chiffrage(cle,mot))
+    else :
+        print(fonction_chiffrage(-cle, mot))
+
+chiffrement_Cesar()
+
 
