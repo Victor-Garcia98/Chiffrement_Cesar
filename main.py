72594563f983082da35a22ac0383e8225c86bde8
Antonin <antonin.chiarotto.1@ens.etsmtl.ca>
2025-01-30 16:45:33 -0500

    partie 1 finale

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import string
 
 alphabet = string.ascii_lowercase
 
+def lecture_fichier(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        texte = file.read()
+    return texte
+
+
 def fonction_chiffrage(cle, mot):
     texte_chiffre = ""
 
@@ -20,8 +26,8 @@ def chiffrement_Cesar() :
     cle = int(input("Entrez la clé : "))
     if input("Avez vous un fichier texte ou une phrase ? (t/p): ") == 't':
         titre_fichier = input("Entrez le nom du fichier : ")
-        #mot = lecture_fichier(titre_fichier)
-        #mot = mot.lower()
+        mot = lecture_fichier(titre_fichier)
+        mot = mot.lower()
     else :
         mot = str(input("Entrez la phrase : "))
         mot = mot.lower()
@@ -35,8 +41,4 @@ chiffrement_Cesar()
 
 
 
-def lecture_fichier(file_name):
-    with open(file_name, 'r', encoding='utf-8') as file:
-        texte = file.read()
-    return texte
 
