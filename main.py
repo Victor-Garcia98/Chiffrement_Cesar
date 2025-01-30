817c610f1e5802b42d192e3130293627894e169e
VictorGarcia <victor.garcia.1@ens.etsmtl.ca>
2025-01-30 15:47:32 -0500

    Création et premier commit de la fonction de déchiffrage

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-flag = True
-""" test de push sur branche"""
+import string
+
+alphabet = string.ascii_lowercase
+text = 'uftu uftu'
+cle = -1
+
+def dechiffrer_texte(mot,cle):
+    nouveau_mot=''
+    for char in mot:
+        if char in alphabet:
+            modif = alphabet.find(char) + cle
+            lettre = alphabet[modif]
+            nouveau_mot = nouveau_mot + lettre
+        else :
+            nouveau_mot = nouveau_mot + char
+
+    return nouveau_mot
+
+
