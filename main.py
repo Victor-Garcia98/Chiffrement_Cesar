9ac7b4a8bde4ad608f3cd2eb725c4983ecefba5f
Antonin <antonin.chiarotto.1@ens.etsmtl.ca>
2025-02-20 19:07:37 -0500

    commentaires sur les fonctions

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ alphabet = string.ascii_lowercase
 
 
 
-
+# Fonction lecture du fichier
 def lecture_fichier(file_name):
     with open(file_name, 'r', encoding='utf-8') as file:
         texte = file.read()
     return texte
 
+# Fonction qui lit un fichier contenant une liste de mots et retourne un ensemble de mots en minuscules et sans espaces
 def dictionnaire(file_name='mots_francais.txt'):
     with open(file_name, "r", encoding="utf-8") as f:
         dictionnaire = set(word.strip().lower() for word in f.readlines())
         return dictionnaire
 
+# Fonction qui permet le chiffrage et le déchiffrage d'un texte
 def fonction_chiffrage(cle, mot):
     texte_chiffre = ""
 
@@ -34,7 +36,7 @@ def fonction_chiffrage(cle, mot):
         texte_chiffre += new_character
     return texte_chiffre
 
-
+# Fonction d'appel principal qui permet d'interagir avec l'utilisateur pour le chiffrement César
 def chiffrement_Cesar() :
     cle = int(input("Entrez la clé : ")) % 25
     mot = interrogation()
@@ -44,6 +46,8 @@ def chiffrement_Cesar() :
     else :
         print(fonction_chiffrage(-cle, mot))
 
+
+# Fonction qui permet de réaliser la méthode du bruteforce
 def bruteforce() :
     mot = interrogation()
     essais = []
@@ -64,6 +68,7 @@ def bruteforce() :
 
     print(bonneCle)
 
+# Fonction qui interragit avec l'utilisateur pour le chiffrement / déchiffrement César
 def interrogation():
 
     if input("Avez vous un fichier texte ou une phrase ? (t/p): ") == 't':
@@ -75,7 +80,7 @@ def interrogation():
         mot = mot.lower()
     return (mot)
 
-
+# Fonction Menu permettant à l'utilisateur de choisir s'il souhaite chiffrer/déchiffrer ou tuliser la méthode brute force
 def menu() :
     choix = int(input("Veuillez choisir l'action que vous souhaitez réaliser :\n\n1 : Chiffrage / Déchiffrage de texte"
                       "\n2 : Méthode bruteforce pour retrouver la clé d'un texte chiffré\n\nChoix : "))
